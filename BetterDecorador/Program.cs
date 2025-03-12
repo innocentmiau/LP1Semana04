105c883c24ba979335891635695abe14e11e18c6
André <imandreleandro@gmail.com>
2025-03-12 17:04:46 +0000

    Added comments

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -4,11 +4,22 @@ namespace BetterDecorador
 {
     public class Program
     {
+        /// <summary>
+        /// The Main function of our program
+        /// </summary>
+        /// <param name="args">Arguments to generate the proper text for Decor function</param>
         private static void Main(string[] args)
         {
             Console.WriteLine(Decor(args[0], char.Parse(args[1]), int.Parse(args[2])));
         }
 
+        /// <summary>
+        /// Function that returns a string containing the text surrounded by the symbols amount
+        /// </summary>
+        /// <param name="text">The text in the middle</param>
+        /// <param name="symbol">Which symbol will be used</param>
+        /// <param name="amount">The amount of symbols you want</param>
+        /// <returns></returns>
         private static string Decor(string text, char symbol, int amount)
         {
             string symbols = "";
