0b9271d0d46205e399a35ede9d7f5407377c22b5
André <imandreleandro@gmail.com>
2025-03-12 16:50:26 +0000

    Added BetterDecoration project to the solution

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Decor(args[0], char.Parse(args[1]), int.Parse(args[2])));
+            /*
+             *  "Once upon a time" "!" 4
+                !!!! Once upon a time !!!!
+                "Lost forever" "+" 2
+                ++ Lost forever ++
+             */
+        }
+
+        private static string Decor(string text, char symbol, int amount)
+        {
+            string symbols = "";
+            for (int i = 0; i < amount; i++)
+            {
+                symbols += symbol;
+            }
+
+            return $"{symbols} {text} {symbols}";
+        }
+    }
+}
