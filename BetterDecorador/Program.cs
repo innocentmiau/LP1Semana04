c099fb3610ba159b23ce09374d0b924030388925
André <imandreleandro@gmail.com>
2025-03-12 16:50:43 +0000

    Fixed BetterDecorator

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -7,12 +7,6 @@ namespace BetterDecorador
         private static void Main(string[] args)
         {
             Console.WriteLine(Decor(args[0], char.Parse(args[1]), int.Parse(args[2])));
-            /*
-             *  "Once upon a time" "!" 4
-                !!!! Once upon a time !!!!
-                "Lost forever" "+" 2
-                ++ Lost forever ++
-             */
         }
 
         private static string Decor(string text, char symbol, int amount)
